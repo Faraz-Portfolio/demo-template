2640c975af01ce9fdf11246392cb9595233ebf54
Faraz Shaikh <farazzshaikh@gmail.com>
2025-06-11 18:15:43 +0400

    fix demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { repository } from "./package.json";
+import { defineConfig } from "vite";
+import { name } from "./package.json";
 
-function getBaseFromRepo(repo: string): string {
-  const match = repo.match(/github\.com\/[^/]+\/[^/]+/);
-  return match ? `/${match[0].split("/").slice(2).join("/")}/` : "/";
-}
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: getBaseFromRepo(repository.url),
+  base: `/${name}/`,
 });
