ce62ca0ebefa78f3d059c463bbf4b26bc898b364
farazzshaikh <farazzshaikh@gmail.com>
2026-06-28 19:00:51 +0400

    Update template to WebGPU + tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import { Inspector } from "@/Inspector";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend } from "@react-three/fiber";
+import * as THREE from "three/webgpu";
+
+extend(THREE as any);
 
 export default function App() {
   return (
-    <Canvas shadows>
-      <OrbitControls makeDefault />
-      <PerspectiveCamera position={[-5, 5, 5]} makeDefault />
+    <>
+      <Canvas
+        shadows
+        gl={async (props) => {
+          const renderer = new THREE.WebGPURenderer(props as any);
+          await renderer.init();
+          return renderer;
+        }}
+      >
+        <OrbitControls makeDefault />
+        <PerspectiveCamera position={[5, 5, 5]} makeDefault fov={35} />
 
-      <axesHelper />
-      <gridHelper />
-    </Canvas>
+        <axesHelper />
+        <gridHelper />
+
+        <Inspector />
+      </Canvas>
+    </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { name } from "./package.json";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
   base: `/${name}/`,
 });
